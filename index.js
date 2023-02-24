dd6d7da55fa37d7b322c7dc28bb2e543326fab4c
Willy <willymmerlet@gmail.com>
2023-02-24 16:26:01 -0500

    re

--- index.js
+++ index.js
@@ -90,7 +90,6 @@ app.post("/questions/:id/responses", async (req, res) => {
     if (optionIndex !== -1) {
       question.responses[optionIndex]++;
       await question.save();
-      console.log(`Question ${questionId} updated with response: ${response}`);
       res.sendStatus(200);
     } else {
       // Response was not found in the options array
